82c982c74a872501d29fa2f3b3c6ef3ce19cc43a
lightsalt2 <chobulhy@gmail.com>
2026-09-11 15:49:44 +0900

    Add Android app build configuration

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.hymnbox.video"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.hymnbox.video"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
